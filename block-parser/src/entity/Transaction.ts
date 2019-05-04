ac8bc156808e50a80e96fc42c51c04f3060408b2
Dejan Erjavec <dejaner@gmail.com>
2019-05-04 13:44:12 +0200

    Add a simple block parser

---
+++ block-parser/src/entity/Transaction.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+
+@Entity('transaction')
+export class TransactionEntity {
+  @PrimaryColumn()
+  txHash: string;
+
+  @Column()
+  fromAddr: string;
+
+  @Column()
+  toAddr: string;
+
+  @Column()
+  value: string;
+
+  @Column()
+  status: string;
+}
---
+++ block-parser/src/index.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import {createConnection, getRepository} from 'typeorm';
+import {TransactionEntity} from './entity/Transaction';
+import Web3 from 'web3';
+
+createConnection().then(async connection => {
+    console.log('Starting block parser...');
+
+    const web3 = new Web3('ws://localhost:8545');
+    const txRepository = getRepository(TransactionEntity);
+
+    web3.eth.subscribe('newBlockHeaders')
+      .on('data', async data => {
+        console.log('New block: ', data.number);
+        const txHashes = (await web3.eth.getBlock(data.number, true)).transactions;
+
+        txHashes.forEach(async txHash => {
+          const txReceipt = await web3.eth.getTransactionReceipt(txHash.hash);
+          console.log('Processing transaction ', txReceipt.transactionHash);
+
+          let tx: TransactionEntity;
+
+          // Limit processing only to transactions created through our eth-bridge for now
+          try {
+            tx = await txRepository.findOneOrFail({ txHash: txReceipt.transactionHash });
+          } catch (error) {
+            console.log('Tx not found in DB, skipping.');
+            return;
+          }
+
+          tx.status = txReceipt.status ? 'successful' : 'failed';
+          await txRepository.save(tx);
+        });
+      });
+}).catch(error => console.log(error));
